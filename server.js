577e43ac79220a8f665e4fbd254ae93b3909e61f
DESKTOP-M0IEGTT\lynnj <lynn.jaimee@gmail.com>
2020-10-22 15:05:55 -0400

    headers

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const path = require('path');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",  "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials","true");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header(
+        'Access-Control-Expose-Headers', 'X-Access-Token, x-refresh-token'
+    );
+    next();
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
