0d1259b759dcfc705affa60541d9b9182a7b48dd
DESKTOP-M0IEGTT\lynnj <lynn.jaimee@gmail.com>
2020-10-22 11:53:51 -0400

    sigh

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req,res){
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
