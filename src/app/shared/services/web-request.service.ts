6c6169271cdcf9acd9eef94fc926f61bc1f0a8e1
DESKTOP-M0IEGTT\lynnj <lynn.jaimee@gmail.com>
2020-10-22 11:11:23 -0400

    stupid heroku

--- src/app/shared/services/web-request.service.ts
+++ src/app/shared/services/web-request.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
